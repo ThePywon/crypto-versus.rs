f8d5a702425c3559a2c5027578aa7d7db102cc5d
ThePywon <ThePywon@hotmail.com>
2023-05-12 09:28:39 -0400

    Added token lifetime

--- src/api_endpoints/signup.rs
+++ src/api_endpoints/signup.rs
@@ -3,7 +3,7 @@ use hyper::{Body, Request, Response, Method, StatusCode};
 use serde::{Serialize, Deserialize};
 use futures::future::BoxFuture;
 use super::super::database::get_database;
-use mongodb::bson::{doc, Document};
+use mongodb::bson::{doc, Document, DateTime};
 use super::super::hash::hash;
 use sha2::Sha256;
 use base64::{engine::general_purpose, Engine as _};
@@ -44,7 +44,10 @@ pub fn run(req: Request<Body>) -> BoxFuture<'static, Result<Response<Body>, Infa
           let mut hashed_token = [0; 32];
           hash::<Sha256>(&token, &std::env::var("TOKEN_SALT").unwrap(), &mut hashed_token);
           let str_hash_token = general_purpose::STANDARD.encode(&hashed_token);
-          tokens.insert_one(doc! { "user_id": user_id, "value": &str_hash_token }, None).await.unwrap();
+
+          let timestamp = DateTime::now().timestamp_millis() + 300000;
+          
+          tokens.insert_one(doc! { "user_id": user_id, "value": &str_hash_token, "valid_until": timestamp }, None).await.unwrap();
 
           Ok(Response::new(Body::from(token)))
         }
